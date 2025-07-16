502aebe58b7aa666fec6ed96cba85acc4fdcf95e
Shinde Tejal <tejalshinde614@gmail.com>
2025-07-16 13:39:32 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+
+		System.out.println("Addisation ="+(a+b));
+		System.out.println("Multification ="+(a*b));
+		System.out.println("Substraction ="+(a-b));
+		System.out.println("Divisation ="+(a/b));
+		System.out.println("Modulas ="+(a%b));
+}
+}
